6e6654f79e46493142af6ebd3e1cdb1c0d39bf24
Brett Reinhard <breinhard@snapchat.com>
2018-06-29 10:07:37 -0700

    Snappy theme

---
+++ install.sh
@@ -0,0 +1,1 @@
+./generate_patch.sh /Applications/Chat.app/Contents/Resources/electron.asar_BACKUP emojis_example.json && cp out/theme/electron.asar /Applications/Chat.app/Contents/Resources/electron.asar && pkill Chat && sleep 1 && open /Applications/Chat.app --args -AppCommandLineArg
