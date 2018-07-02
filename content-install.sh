08f40af12151421b50bc75cbedf4776e9105dea6
Brett Reinhard <brettreinhard@gmail.com>
2018-07-02 07:57:54 -0700

    Update content-install.sh

--- content-install.sh
+++ content-install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-
-wget https://github.com/BaReinhard/Slack-Theme-for-Hangouts-Chat/raw/compact/electron.asar ~/Desktop/NEWASAR.asar
+cp /Applications/Chat.app/Contents/Resources/electron.asar /Applications/Chat.app/Contents/Resources/electron.asar_BU
+wget https://github.com/BaReinhard/Slack-Theme-for-Hangouts-Chat/raw/compact/electron.asar -P /Applications/Chat.app/Contents/Resources/electron.asar
+pkill Chat
+sleep 1
+open /Applications/Chat.app --args -AppCommandLineArg
